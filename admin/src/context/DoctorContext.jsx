e1fbddb666cf8d0b3c1dff8614b869d154d14e55
rahul-6355 <rk4032422@gmail.com>
2025-08-08 16:32:21 +0530

    Update DoctorContext.jsx

--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -7,7 +7,7 @@ export const DoctorContext = createContext();
 
 const DoctorContextProvider = (props) => {
 
-    const backendUrl = import.meta.env.VITE_BACKEND_URL
+    const backendUrl = 'https://healthcare-backend-4m9d.onrender.com'
 
     const [dToken,setDToken] = useState(localStorage.getItem('dToken') ? localStorage.getItem('dToken') : '')
     const [appointments , setAppointments] = useState([])
